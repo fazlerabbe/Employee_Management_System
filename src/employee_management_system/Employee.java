ba27fba58f478c570679f758bf5e0cae70c7beef
fazlerabbe <58098907+fazlerabbe@users.noreply.github.com>
2020-12-12 21:04:55 +0600

    first commit

---
+++ src/employee_management_system/Employee.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee_management_system;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Employee {
+
+    private String id;
+    private String name;
+    private String email;
+    private String address;
+    private String post;
+
+    Employee(String id, String name, String email, String address, String post) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.post = post;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+  void Add_employee()
+  {
+      
+  }
+  void Update_employee()
+  {
+      
+  }
+  void remove_employee()
+  {
+      
+  }
+  void view_employee()
+  {
+      
+  }
+}
